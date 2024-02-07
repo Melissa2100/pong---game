e77bcb7a9c49d4dcb0c2f565b528b5ab402a2183
Blessing Awak <melissasignature1@gmail.com>
2024-02-07 16:26:57 +0100

    commit

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const gameHeight = gameBoard.height;
 const boardBackground = "forestgreen";
 const paddle1Color = "lightblue";
 const paddle2Color = "red";
-const ballColor = "yellow";
 const paddleBorder = "black";
+const ballColor = "yellow";
 const ballRadius = 12.5;
 const paddleSpeed = 50;
 let intervalID;
@@ -17,6 +17,38 @@ let ballX = gameWidth / 2;
 let ballY = gameHeight / 2;
 let ballXDirextion = 0;
 let ballYDirextion = 0;
-let palyer1Score = 0;
-let palyer2Score = 0;
-y
+let player1Score = 0;
+let player2Score = 0;
+let paddle1 = {
+  width: 25,
+  height: 100,
+  x: 0,
+  y: 0,
+};
+let paddle2 = {
+  width: 25,
+  height: 100,
+  x: gameWidth - 25,
+  y: gameHeight - 100,
+};
+
+window.addEventListener("keydown", changeDirection);
+resetBtn.addEventListener("click", resetGame);
+
+gameStart();
+
+function gameStart() {};
+function nextTick() {};
+function clearBoard() {};
+function drawPaddles() {
+ ctx.strokeStyle = 
+};
+function createBall() {};
+function moveBall() {};
+function drawBall() {};
+function checkCollision() {};
+function changeDirection() {};
+function updateScore() {};
+function resetGame() {};
+
+
