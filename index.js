edf22d0c401962e97dc8fea84cb47688ed0275e2
Blessing Awak <melissasignature1@gmail.com>
2024-02-07 17:25:24 +0100

    commit

--- index.js
+++ index.js
@@ -111,7 +111,7 @@ function checkCollision() {
   }
   if (ballX <= paddle1.x + paddle1.width + ballRadius) {
     if (ballY > paddle1.y && ballY < paddle1.y + paddle1.height) {
-      ballX = paddle1.x + paddle1.width + ballRadius;
+      ballX = (paddle1.x + paddle1.width) + ballRadius; //if ball gets stuck
       ballXDirection *= -1;
       ballSpeed += 1;
     }
